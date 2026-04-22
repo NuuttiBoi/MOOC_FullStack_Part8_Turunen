706f4acb13dea2326731d76312dfad9e89680843
NuuttiBoi <104026912+NuuttiBoi@users.noreply.github.com>
2026-04-22 16:48:34 +0300

    .

--- library-frontend/src/components/Recommend.jsx
+++ library-frontend/src/components/Recommend.jsx
@@ -2,33 +2,32 @@ import { useQuery } from '@apollo/client/react'
 import { ALL_BOOKS, ME } from '../queries'
 
 const Recommended = ({ show }) => {
-    const booksResult = useQuery(ALL_BOOKS)
-    const curUserResult = useQuery(ME)
+    const booksQuery = useQuery(ALL_BOOKS)
+    const meQuery = useQuery(ME)
     if (!show) {
         return null
-    }
-    if (booksResult.loading || curUserResult.loading) {
+    }if (booksQuery.loading || meQuery.loading) {
         return <div>loading...</div>
     }
-    if (booksResult.error) {
-        return <div>Error: {booksResult.error.message}</div>
+    if (booksQuery.error) {
+        return <div>error: {booksQuery.error.message}</div>
     }
-    if (curUserResult.error) {
-        return <div>Error: {curUserResult.error.message}</div>
+    if (meQuery.error) {
+        return <div>error: {meQuery.error.message}</div>
     }
-    const books = booksResult.data?.allBooks ?? []
-    const favoriteGenre = curUserResult.data?.me?.favoriteGenre
-    const recommendedBooks = books.filter((book) =>
-        book.genres.includes(favoriteGenre)
-    )
+
+    const books = booksQuery.data.allBooks
+    const user = meQuery.data.me
+    const favoriteGenre = user.favoriteGenre
+    const filteredBooks = books.filter(book => {
+        return book.genres.includes(favoriteGenre)
+    })
     return (
         <div>
             <h2>recommendations</h2>
-
             <p>
                 books in your favorite genre <strong>{favoriteGenre}</strong>
             </p>
-
             <table>
                 <tbody>
                 <tr>
@@ -36,17 +35,15 @@ const Recommended = ({ show }) => {
                     <th>author</th>
                     <th>published</th>
                 </tr>
-                {recommendedBooks.map((book) => (
+                {filteredBooks.map(book => (
                     <tr key={book.title}>
                         <td>{book.title}</td>
                         <td>{book.author.name}</td>
                         <td>{book.published}</td>
                     </tr>
                 ))}
-                </tbody>
-            </table>
+                </tbody></table>
         </div>
-    )
+        )
 }
-
 export default Recommended
